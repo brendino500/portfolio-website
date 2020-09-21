5cc2322659a16c72501a721bdaab154fbcafc14c
brendino500 <brenda.ty@live.com>
2020-09-21 12:20:07 +0100

    adding home parallax page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+
+import HomeParallax from './components/homeParallax/HomeParallax'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <main>
+        <Switch>
+          <Route exact path="/" component={HomeParallax} />
+        </Switch>
+      </main>
+    </BrowserRouter>
+  )
+}
+
+export default App
