038915eb2e4ef9e9fb296bc4ae4d35a8b89e249c
brendino500 <brenda.ty@live.com>
2020-09-21 17:01:37 +0100

    navbar

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Typography } from '@material-ui/core'
 
 class About extends React.Component {
   render() {
     return (
+      <Typography>
       <h1>About</h1>
+      <p>My interest in coding began when I was teaching a student the piano and I couldn’t find online resources which were fun, engaging and informative. I wanted to create a music theory-based app but didn’t have the skills. After having enrolled and completed the Software Engineering Immersive course at General Assembly, I have now learnt a strong foundation of modern programming languages and frameworks.
+
+      I am a hard-working individual and years of experience in the workplace have led me to start up my own coffee business, lead teams, hire and train new staff, open coffee shops and operate a whole roastery. I am looking for my first role as a developer where I can share my creativity and learn from others.
+      </p>
+      </Typography>
     )
   }
 }
