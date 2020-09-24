c49413288399e9d3b4db6fc7501c215dd8345c83
brendino500 <brenda.ty@live.com>
2020-09-24 11:01:08 +0100

    reactathon page added

---
+++ src/components/Reactathon.js
@@ -0,0 +1,43 @@
+import React from 'react'
+
+import { makeStyles } from '@material-ui/core/styles'
+import { Container, Box, Grid, Typography } from '@material-ui/core'
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1
+  },
+  text: {
+    flexGrow: 1,
+    fontFamily: 'Work Sans',
+    color: '#d3c9c0',
+    fontSize: '20px',
+    letterSpacing: '1px'
+  }
+}))
+
+function Reactathon() {
+  const classes = useStyles()
+
+  return (
+    <Container maxWidth="md">
+      <Box>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="center"
+        >
+          <br />
+          <Typography className={classes.text}>
+            48h Reactathon
+          </Typography>
+          <br />
+          <Typography className={classes.text}>
+            48h Reactathon project pulling from 5 APIs
+          </Typography>
+        </Grid>
+      </Box>
+    </Container>
+  )
+}
