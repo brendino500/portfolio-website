fa4cc958cc3aede2f98298380f8727c77dc0170d
brendino500 <brenda.ty@live.com>
2020-11-08 17:48:18 +0000

    accordion bug fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,39 +1,39 @@
-import React from "react";
+import React from 'react'
 
-import { makeStyles } from "@material-ui/core/styles";
-import { Typography } from "@material-ui/core";
+import { makeStyles } from '@material-ui/core/styles'
+import { Typography } from '@material-ui/core'
 
 const useStyles = makeStyles((theme) => ({
   footer: {
-    fontFamily: "Neuton",
-    color: "#d3c9c0",
+    fontFamily: 'Neuton',
+    color: '#d3c9c0',
     fontSize: 15,
-    marginLeft: "20%",
+    marginLeft: '20%',
     letterSpacing: 2,
-    [theme.breakpoints.between("xs", "sm")]: {
+    [theme.breakpoints.between('xs', 'sm')]: {
       fontSize: 10,
-      marginLeft: "10%",
+      marginLeft: '10%',
     },
-    [theme.breakpoints.between("sm", "md")]: {
+    [theme.breakpoints.between('sm', 'md')]: {
       fontSize: 10,
-      marginLeft: "5%",
+      marginLeft: '5%',
     },
-    [theme.breakpoints.between("md", "lg")]: {
+    [theme.breakpoints.between('md', 'lg')]: {
       fontSize: 15,
-      marginLeft: "12%",
+      marginLeft: '12%',
     },
   },
-}));
+}))
 
 function Footer() {
-  const classes = useStyles();
+  const classes = useStyles()
 
   return (
     <Typography className={classes.footer}>
       I made this website all by myself using: React-Spring, Robin Dela's Hover
       Effect, React Hooks and Material UI.
     </Typography>
-  );
+  )
 }
 
-export default Footer;
+export default Footer
